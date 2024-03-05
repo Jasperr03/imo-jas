812a226cc5e2a2536b753d9c8294114ec65608d8
Jasperr03 <jasperellevera03@gmail.com>
2024-03-05 19:14:55 +0800

    modified bootstrap

--- about.php
+++ about.php
@@ -3,8 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="assets/img/creative-pencil-logo-design-vector-removebg-preview.png" rel="icon">
     <link rel="stylesheet" href="css/mystyle.css">
 </head>
