62533027625cd7f84666b74025afd80a5d6fab0b
Jasperr03 <jasperellevera03@gmail.com>
2024-03-05 16:08:48 +0800

    modified navlink

--- components/navlink.php
+++ components/navlink.php
@@ -1,8 +1,8 @@
 
 <div>
-<div class="topnav-right">
-            <a href="index.php">Home</a>
-            <a href='gallery.php'>Gallery</a>
-            <a href="about.php">About</a>   
-        </div>
+    <div class="topnav-right">
+        <a href="index.php">Home</a>
+        <a href='gallery.php'>Gallery</a>
+        <a href="about.php">About</a>   
+    </div>
 </div>
