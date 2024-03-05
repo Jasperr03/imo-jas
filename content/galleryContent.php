678388bdee98df6e0645e26f097b5cf5ac16260c
Jasperr03 <jasperellevera03@gmail.com>
2024-03-05 16:36:46 +0800

    added gallery contents

--- content/galleryContent.php
+++ content/galleryContent.php
@@ -5,28 +5,36 @@
             <a target="_blank" href="img_5terre.jpg">
                <img src="assets/img/close-up-artist-sketch.jpg" alt="Cinque Terre" width="600" height="400">
             </a>
-            <div class="desc">Add a description of the image here</div>
+            <div class="desc">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+         </div>
       </div>
 
       <div class="gallery">
          <a target="_blank" href="img_forest.jpg">
             <img src="assets/img/close-up-artist-sketch.jpg" alt="Forest" width="600" height="400">
          </a>
-         <div class="desc">Add a description of the image here</div>
+         <div class="desc">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+         </div>
       </div>
 
       <div class="gallery">
          <a target="_blank" href="img_lights.jpg">
             <img src="assets/img/close-up-artist-sketch.jpg" alt="Northern Lights" width="600" height="400">
          </a>
-         <div class="desc">Add a description of the image here</div>
+         <div class="desc">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+         </div>
       </div>
 
       <div class="gallery">
          <a target="_blank" href="img_mountains.jpg">
             <img src="assets/img/close-up-artist-sketch.jpg" alt="Mountains" width="600" height="400">
          </a>
-         <div class="desc">Add a description of the image here</div>
+         <div class="desc">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+         </div>
       </div>
    </div>
 </div>
